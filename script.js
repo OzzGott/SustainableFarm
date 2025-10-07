9dbf878da941fad31a3fe73c90deb6ee4fc810af
Osvald osmo <osmo@itu.dk>
2025-10-07 12:50:04 +0200

    translations, graphics and q's

--- script.js
+++ script.js
@@ -16,44 +16,67 @@ const restartBtn = document.getElementById('restart-btn');
 
 const questions = [
     {
-        text: "Du kan ikke gå alle vegne, hvad vil du køre i?",
+        text: "Du skal plante nogle afgrøder, hvad kunne du tænke dig??",
         options: [
-            { name: "Ny traktor (Diesel)", cost: 400, profit: 300, co2: 200, img: "https://www.profi.co.uk/wp-content/uploads/sites/8/2022/12/New-Holland-T7-Methane-Power-LNG-Tractor-lead.jpg" },
-            { name: "Ny traktor (Elektrisk)", cost: 600, profit: 250, co2: 50, img: "https://grouchyfarmer.com/wp-content/uploads/2023/09/screenshot-2023-09-25-at-5.31.41-am.png" },
-            { name: "Brugt traktor (Diesel)", cost: 250, profit: 150, co2: 100, img: "https://m.atcdn.co.uk/a/media/w600/6c66b3871f6643c9b548d0abcab69e12.jpg" }
+            { name: "Raps", cost: 12_000, profit: 18_000, co2: 700, img: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Brassica_napus_LC0027.jpg" },
+            { name: "Hvede", cost: 10_000, profit: 15_000, co2: 500, img: "https://s3-eu-west-1.amazonaws.com/yara-links/srgt.jpg" },
+            { name: "Bælgplanter", cost: 9_000, profit: 12_000, co2: 200, img: "https://frdk.dk/wp-content/uploads/2023/03/Hesteboenner_san.jpg" }
         ]
     },
     {
         text: "Dine afgrøder har brug for en hånd, hvilken gødning bruger du?",
         options: [
-            { name: "Kemisk gødning", cost: 300, profit: 500, co2: 400, img: "https://gratisgødning.dk/wp-content/uploads/2015/09/kunst.jpg" },
-            { name: "Organisk gødning", cost: 350, profit: 400, co2: 150, img: "https://haveselskabet.dk/media/pmqg2abi/intro-istock-545582750_0.jpg" },
-            { name: "Ingen gødning", cost: 0, profit: 100, co2: 0, img: "https://static.thenounproject.com/png/4476033-200.png" }
+            { name: "Kunstgødning", cost: 5_000, profit: 5_000, co2: 400, img: "https://gratisgødning.dk/wp-content/uploads/2015/09/kunst.jpg" },
+            { name: "Økologisk organisk gødning", cost: 6_000, profit: 4_000, co2: 100, img: "https://haveselskabet.dk/media/pmqg2abi/intro-istock-545582750_0.jpg" },
+            { name: "Ingen gødning", cost: 0, profit: 2_000, co2: 0, img: "https://static.thenounproject.com/png/4476033-200.png" }
         ]
     },
     {
-        text: "Lidt ekstra strøm ville være rart, hvilken slags vil du invistere i?",
+        text: "Pesticider reducerer din profit, hvad vil du sprøjte med?",
         options: [
-            { name: "Solpaneler", cost: 500, profit: 300, co2: -50, img: "https://cdn.britannica.com/94/192794-050-3F3F3DDD/panels-electricity-order-sunlight.jpg" },
-            { name: "Dieselgenerator", cost: 200, profit: 200, co2: 300, img: "https://shop74079.sfstatic.io/upload_dir/pics/produkt-billeder/vaerktoej/Walter-stahl/generator/Walter-Stahl-PR10000D/_thumbs/1.w1200.jpg" },
+            { name: "Konventionel sprøjtning", cost: 4_000, profit: 5_000, co2: 400, img: "https://okonu.dk/img/asset/YXJ0aWNsZXMvMjAxOS9Qb2xpdGlrX1VkdmlrbGluZy9wZXN0aWNpZGVyLXNwcm9qdGVzLS0tY29sb3VyYm94LmpwZw==/pesticider-sprojtes---colourbox.jpg?fit=crop-49-68-1&w=2351&h=1322&s=87f3df8fa17803db1614ee934b7553e4" },
+            { name: "Biologisk sprøjtning", cost: 5_000, profit: 3_000, co2: 100, img: "https://mim.dk/media/ceql4wl0/hjemmeside_kemi-og-drikkevand_kemikalier.jpg?width=960" },
+            { name: "Så slemt kan de da ikke være, ingen pesticider", cost: 0, profit: 1_000, co2: 0, img: "https://shop65489.sfstatic.io/upload_dir/shop/_thumbs/Vandkande_9-liter.w610.h610.backdrop.jpg" }
+        ]
+    },
+    {
+        text: "Det bliver noget juks at bare gå rundt på marken, så hvad vil du køre i?",
+        options: [
+            { name: "Ny effektiv traktor", cost: 50_000, profit: 15_000, co2: 1000, img: "https://www.profi.co.uk/wp-content/uploads/sites/8/2022/12/New-Holland-T7-Methane-Power-LNG-Tractor-lead.jpg" },
+            { name: "Brugt traktor", cost: 20_000, profit: 8_000, co2: 700, img: "https://m.atcdn.co.uk/a/media/w600/6c66b3871f6643c9b548d0abcab69e12.jpg" },
+            { name: "Jeg lejer en traktor når jeg skal bruge den", cost: 10_000, profit: 5_000, co2: 500, img: "https://grouchyfarmer.com/wp-content/uploads/2023/09/screenshot-2023-09-25-at-5.31.41-am.png" }
+        ]
+    },
+    {
+        text: "Nogle dyr ville være på sin plads, men hvilke?",
+        options: [
+            { name: "Malkekøer", cost: 30_000, profit: 20_000, co2: 2_000, img: "https://cdn.prod.website-files.com/649596b20e583843b3d32617%2F678d1d16378686294893be5f_Bannervideo%20test-poster-00001.jpg" },
+            { name: "Får", cost: 15_000, profit: 10_000, co2: 500, img: "https://cdn.britannica.com/94/192794-050-3F3F3DDD/panels-electricity-order-sunlight.jpg" },
+            { name: "Dyr må blive en anden god gang", cost: 0, profit: 0, co2: 0, img: "https://bolius-prod.s3.amazonaws.com/_processed_/e/6/csm_20120907-020938-3-2000x1264web-Lundtoftevej_2bf86a061c.jpg" }
+        ]
+    },
+    {
+        text: "En ekstra energikilde ville være rart, hvilken slags vil du investere i?",
+        options: [
+            { name: "Min egen vindmølle", cost: 40_000, profit: 5_000, co2: -500, img: "https://cdn.prod.website-files.com/649596b20e583843b3d32617%2F678d1d16378686294893be5f_Bannervideo%20test-poster-00001.jpg" },
+            { name: "Solpaneler", cost: 25_000, profit: 3_000, co2: -300, img: "https://cdn.britannica.com/94/192794-050-3F3F3DDD/panels-electricity-order-sunlight.jpg" },
             { name: "Jeg tager bare strømmen fra væggen", cost: 0, profit: 0, co2: 0, img: "https://bolius-prod.s3.amazonaws.com/_processed_/e/6/csm_20120907-020938-3-2000x1264web-Lundtoftevej_2bf86a061c.jpg" }
         ]
-    },
-    {
-        text: "Dine marker skal vandes, hvordan har du lyst til at gøre det?",
+    },    {
+        text: "Markerne skal jo nok vandes, hvordan burde man gøre det?",
         options: [
-            { name: "Drypvanding", cost: 300, profit: 250, co2: 50, img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVFhMhZ-7msvuCJc16eOrqW5kg_gu76pjT-g&s" },
-            { name: "Jeg vander dem da bare selv", cost: 50, profit: 50, co2: 0, img: "https://shop65489.sfstatic.io/upload_dir/shop/_thumbs/Vandkande_9-liter.w610.h610.backdrop.jpg" }
+            { name: "Automatisk vandingssystem", cost: 20_000, profit: 10_000, co2: 200, img: "https://cdn.prod.website-files.com/649596b20e583843b3d32617%2F678d1d16378686294893be5f_Bannervideo%20test-poster-00001.jpg" },
+            { name: "Manuel vanding", cost: 5_000, profit: 4_000, co2: 50, img: "https://cdn.britannica.com/94/192794-050-3F3F3DDD/panels-electricity-order-sunlight.jpg" },
+            { name: "Det regner sgu så meget i Danmark, ingen vanding", cost: 0, profit: 1_000, co2: 0, img: "https://bolius-prod.s3.amazonaws.com/_processed_/e/6/csm_20120907-020938-3-2000x1264web-Lundtoftevej_2bf86a061c.jpg" }
+        ]
+    },    {
+        text: "Jorden skal ?",
+        options: [
+            { name: "Automatisk vandingssystem", cost: 20_000, profit: 10_000, co2: 200, img: "https://cdn.prod.website-files.com/649596b20e583843b3d32617%2F678d1d16378686294893be5f_Bannervideo%20test-poster-00001.jpg" },
+            { name: "Manuel vanding", cost: 5_000, profit: 4_000, co2: 50, img: "https://cdn.britannica.com/94/192794-050-3F3F3DDD/panels-electricity-order-sunlight.jpg" },
+            { name: "Det regner sgu så meget i Danmark, ingen vanding", cost: 0, profit: 1_000, co2: 0, img: "https://bolius-prod.s3.amazonaws.com/_processed_/e/6/csm_20120907-020938-3-2000x1264web-Lundtoftevej_2bf86a061c.jpg" }
         ]
     },
-    {
-        text: "Du skal plante nogle afgrøder, hvad kunne du tænke dig??",
-        options: [
-            { name: "Raps", cost: 400, profit: 600, co2: 400, img: "https://via.placeholder.com/80" },
-            { name: "Hvede", cost: 300, profit: 400, co2: 200, img: "https://via.placeholder.com/80" },
-            { name: "Bælgplanter", cost: 250, profit: 350, co2: 50, img: "https://via.placeholder.com/80" }
-        ]
-    }
 ];
 
 function updateStatus() {
@@ -87,8 +110,8 @@ function renderQuestion() {
       <img src="${option.img}" alt="${option.name}">
       <div>
         <strong>${option.name}</strong><br>
-        Cost: $${option.cost}<br>
-        Profit: $${option.profit}<br>
+        Pris: ${option.cost},-<br>
+        Profit: ${option.profit},-<br>
         CO₂e: ${option.co2} kg
       </div>
     `;
@@ -100,7 +123,6 @@ function renderQuestion() {
 
 function selectOption(option) {
     if (option.cost > budget) return;
-
     budget -= option.cost;
     totalProfit += option.profit;
     totalCO2 += option.co2;
@@ -124,6 +146,10 @@ restartBtn.addEventListener('click', () => {
     updateStatus();
     renderQuestion();
 });
+
+function nextQuestion(percent){
+
+}
 
 // Initialize
 updateStatus();
